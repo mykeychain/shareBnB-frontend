34c0c897b9d36b10e2e74794cada3fe006883af5
Nathaniel Cuenca <ncuenca.nac@gmail.com>
2021-08-25 13:29:51 -0700

    rounded carousel pictures

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,10 +1,11 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import './ListingCard.css'
 
 export default function ListingCard({ listing }) {
     return (
         <div className="ListingCard col-3">
-            <div className="ListingCard">
+            <div className="ListingCardCarousel">
                 <Carousel showThumbs={false} showStatus={false}>
                     {listing.photos.map(photo => (
                         <div key={photo.id}>
