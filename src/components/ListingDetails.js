cb414482224dff9b9d1bb2c1da75a7084c0c3510
Nathaniel Cuenca <ncuenca.nac@gmail.com>
2021-08-26 21:41:15 -0700

    re-styled listing details page

--- src/components/ListingDetails.js
+++ src/components/ListingDetails.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Carousel } from "react-responsive-carousel";
 import { useParams } from 'react-router-dom';
 import ShareBnBApi from "../api";
+import './ListingDetails.css';
 
 export default function ListingDetails() {
     const { id } = useParams();
@@ -20,21 +21,27 @@ export default function ListingDetails() {
 
     if (listing && host) {
         return (
-            <div className='ListingDetails container'>
-                <h1 className='mt-3'>{listing.title}</h1>
-                <h4>Hosted by: {host.first_name} {host.last_name} - {host.username}</h4>
-                <div className="ListingCardCarousel mb-4">
-                    <Carousel showThumbs={false} showStatus={false}>
-                        {listing.photos.map(photo => (
-                            <div key={photo.id}>
-                                <img src={photo.url} alt=""/>
-                            </div>
-                        ))}
-                    </Carousel>
+            <div className='ListingDetails container mt-4'>
+                <div className='row'>
+                    <div className='col-6'>
+                        <div className="ListingCardCarousel mb-4">
+                            <Carousel showStatus={false}>
+                                {listing.photos.map(photo => (
+                                    <div key={photo.id}>
+                                        <img src={photo.url} alt=""/>
+                                    </div>
+                                ))}
+                            </Carousel>
+                        </div>
+                        </div>
+                    <div className='col-6'>
+                        <h1>{listing.title}</h1>
+                        <h2>{listing.address}</h2>
+                        <h2>Hi <small class='text muted'>${listing.price} /night</small></h2>
+                        <h4>Hosted by: {host.first_name} {host.last_name} - {host.username}</h4>
+                        <h4>{listing.details}</h4>
+                    </div>
                 </div>
-                <h2>${listing.price} /night</h2>
-                <h2>{listing.address}</h2>
-                <h4>{listing.details}</h4>
             </div>
         )
     } else {
